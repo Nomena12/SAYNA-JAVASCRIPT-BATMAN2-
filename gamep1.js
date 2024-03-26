8e06a5fe45e68dbe8f27bdf634203b44ce889ec3
Ramarolahy <nomenaramarolahy@yahoo.com>
2024-03-26 04:57:17 +0100

    amelioration des fichiers js

--- gamep1.js
+++ gamep1.js
@@ -1,3 +1,18 @@
+ /*  if(a < 21){
+        a+=1;
+
+    const img = document.querySelector('img[alt="Batgame3"]');
+    img.setAttribute("src",`Illustrations/Batgame_${a}.png`);
+    }else{a = 21} ; */
+
+    //let n = 6;
+      //  const para = document.querySelector(".down");
+      //  div.textContent = n;
+
+//let a = 3;
+
+
+
 window.addEventListener('scroll', (e)=>{
     const aside = document.querySelector(".aside");
    
@@ -9,17 +24,34 @@ window.addEventListener('scroll', (e)=>{
 
 });
 
-let a = 3;
 
-
+let n = 1 ;
+let totQuest = 15;
         
             const btn = document.querySelector("button");
             btn.addEventListener("click",()=>{
-    if(a < 21){
-        a+=1;
+                
+                
+  
 
-    const img = document.querySelector('img[alt="Batgame3"]');
-    img.setAttribute("src",`Illustrations/Batgame_${a}.png`);
-    }else{a = 21}                       
+    const box = document.querySelector(".out");
+    const box1 = document.querySelector(".out1");
+    box.style.display = "none";
+    box1.style.display = "block";
+
+    if(n < 15){
+n += 1
+     const para = document.querySelector(".down");
+     para.textContent = n +`/${totQuest}`;}else{n = 15}
+ 
+    
+
+    
+    
         });
 
+       
+
+      
+
+        
