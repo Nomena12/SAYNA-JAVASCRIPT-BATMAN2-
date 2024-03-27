5effe5cd194f586c3c270114d57b906dce5311ad
Ramarolahy <nomenaramarolahy@yahoo.com>
2024-03-27 05:02:09 +0100

    amelioration du fichier js et css

--- gamep1.js
+++ gamep1.js
@@ -39,6 +39,48 @@ let totQuest = 15;
     box.style.display = "none";
     box1.style.display = "block";
 
+
+            btn.addEventListener("click",()=>{
+                const box1 = document.querySelector(".out1");
+                const box2 = document.querySelector(".out2");
+                box1.style.display = "none";
+                box2.style.display = "block";
+
+
+                btn.addEventListener("click",()=>{
+                    const box2 = document.querySelector(".out2");
+                    const box3 = document.querySelector(".out3");
+                    box2.style.display = "none";
+                    box3.style.display = "block";
+
+                    btn.addEventListener("click",()=>{
+                        const box3 = document.querySelector(".out3");
+                        const box4 = document.querySelector(".out4");
+                        box3.style.display = "none";
+                        box4.style.display = "block";
+
+                        btn.addEventListener("click",()=>{
+                            const box4 = document.querySelector(".out4");
+                            const box5 = document.querySelector(".out5");
+                            box4.style.display = "none";
+                            box5.style.display = "block";
+
+                            btn.addEventListener("click",()=>{
+                                const box5 = document.querySelector(".out5");
+                                const box6 = document.querySelector(".out6");
+                                box5.style.display = "none";
+                                box6.style.display = "block";
+
+                            });
+                            
+
+                        });
+
+                    });
+
+                });
+            });
+
     if(n < 15){
 n += 1
      const para = document.querySelector(".down");
